9cc2dc06f10ee95676d2d00dbfd3936f447b1b9b
monayemchy <monayemchy@gmail.com>
2025-09-06 20:16:09 -0400

    update exam route

--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::resource('questions', QuestionController::class);
-Route::resource('exam', ExamController::class);
+Route::resource('exams', ExamController::class);
